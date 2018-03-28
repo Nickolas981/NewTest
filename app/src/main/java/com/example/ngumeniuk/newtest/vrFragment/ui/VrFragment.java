0a37a7107d030c097ab29c090fd6540eb133713c
Nickolas981 <kolin98111@gmail.com>
2018-03-28 20:55:00 +0300

    Created fragment for vr

---
+++ app/src/main/java/com/example/ngumeniuk/newtest/vrFragment/ui/VrFragment.java
@@ -0,0 +1,43 @@
+package com.example.ngumeniuk.newtest.vrFragment.ui;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ngumeniuk.newtest.R;
+public class VrFragment extends Fragment {
+
+
+    public VrFragment() {
+    }
+
+    public static VrFragment newInstance() {
+        return new VrFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_vr, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
